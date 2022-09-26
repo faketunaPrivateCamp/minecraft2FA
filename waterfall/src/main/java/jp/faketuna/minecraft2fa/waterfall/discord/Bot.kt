6a8c8b48c1b443a175aede3445250c6a44149c3c
faketuna <faketuna.st@gmail.com>
2022-09-27 01:36:52 +0900

    DiscordBotクラスを継承したクラスからもjdaインスタンスを取得できるようにする。 #4 Related #4

--- waterfall/src/main/java/jp/faketuna/minecraft2fa/waterfall/discord/Bot.kt
+++ waterfall/src/main/java/jp/faketuna/minecraft2fa/waterfall/discord/Bot.kt
@@ -1,5 +1,28 @@
 package jp.faketuna.minecraft2fa.waterfall.discord
 
 import jp.faketuna.minecraft2fa.shared.discord.DiscordBot
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 
-class Bot(private val token: String, private val intents: Int): DiscordBot(token, intents){}
+class Bot(private val token: String, private val intents: Int): DiscordBot(token, intents){
+    private var jda: JDA = DiscordObject.getJDAInstance()
+
+    init {
+    }
+
+    override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+        if(event.name == "test"){
+            event.reply("Bot name: ${jda.status.name}")
+                .setEphemeral(true)
+                .queue()
+        }
+    }
+
+    override fun onMessageReceived(event: MessageReceivedEvent) {
+        if(event.message.contentRaw == "test"){
+            event.message.reply("Bot status: ${jda.status.name}")
+                .queue()
+        }
+    }
+}
