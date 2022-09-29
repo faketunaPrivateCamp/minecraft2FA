f71c5a8e5aefa86e02f433affc30569fd007f1bc
faketuna <faketuna.st@gmail.com>
2022-09-29 10:49:26 +0900

    (Fix) Tokenが正しくなかった場合にプラグインを起動しないようにした

--- paper/src/main/java/jp/faketuna/minecraft2fa/paper/Minecraft2FA.kt
+++ paper/src/main/java/jp/faketuna/minecraft2fa/paper/Minecraft2FA.kt
@@ -3,6 +3,7 @@ package jp.faketuna.minecraft2fa.paper
 import jp.faketuna.minecraft2fa.paper.discord.Bot
 import jp.faketuna.minecraft2fa.paper.manager.PluginInstanceManager
 import jp.faketuna.minecraft2fa.shared.config.ConfigManager
+import net.dv8tion.jda.api.exceptions.InvalidTokenException
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -14,7 +15,14 @@ class Minecraft2FA: JavaPlugin() {
         manager.setConfigManager(ConfigManager())
         if(!Bukkit.spigot().config.getBoolean("settings.bungeecord")){
             val token = manager.getConfigManager(this).getToken()
-            manager.setDiscordBotInstance(Bot(token))
+            try{
+                manager.setDiscordBotInstance(Bot(token))
+            } catch (e: InvalidTokenException){
+                logger.info("§4Your Token is invalid! Check your config.")
+                logger.info("§4Plugin will not start.")
+                Bukkit.getPluginManager().disablePlugin(this)
+                return
+            }
         }
         manager.setPlugin(this)
     }
--- waterfall/src/main/java/jp/faketuna/minecraft2fa/waterfall/Minecraft2FA.kt
+++ waterfall/src/main/java/jp/faketuna/minecraft2fa/waterfall/Minecraft2FA.kt
@@ -3,6 +3,7 @@ package jp.faketuna.minecraft2fa.waterfall
 import jp.faketuna.minecraft2fa.shared.config.ConfigManager
 import jp.faketuna.minecraft2fa.waterfall.discord.Bot
 import jp.faketuna.minecraft2fa.waterfall.manager.PluginInstanceManager
+import net.dv8tion.jda.api.exceptions.InvalidTokenException
 import net.md_5.bungee.api.plugin.Plugin
 
 class Minecraft2FA: Plugin() {
@@ -12,7 +13,14 @@ class Minecraft2FA: Plugin() {
         logger.info("loading plugin")
         manager.setConfigManager(ConfigManager())
         val token = manager.getConfigManager(this).getToken()
-        manager.setDiscordBotInstance(Bot(token))
+        try{
+            manager.setDiscordBotInstance(Bot(token))
+        } catch (e: InvalidTokenException){
+            e.printStackTrace()
+            logger.info("§4Your Token is invalid! Check your config.")
+            logger.info("§4Plugin will not start.")
+            return
+        }
         manager.setPlugin(this)
     }
 
