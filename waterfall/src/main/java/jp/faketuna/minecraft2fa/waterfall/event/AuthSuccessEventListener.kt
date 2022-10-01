8dce09e2e6eb58509330a043d54ce3f6e2e20bf9
faketuna <faketuna.st@gmail.com>
2022-10-01 14:33:05 +0900

    (Add) 認証した際にサーバーに参加してる場合は即座に反映するようになった Related #34

--- waterfall/src/main/java/jp/faketuna/minecraft2fa/waterfall/event/AuthSuccessEventListener.kt
+++ waterfall/src/main/java/jp/faketuna/minecraft2fa/waterfall/event/AuthSuccessEventListener.kt
@@ -1,20 +1,47 @@
 package jp.faketuna.minecraft2fa.waterfall.event
 
+import com.google.common.io.ByteStreams
 import jp.faketuna.minecraft2fa.shared.manager.AuthInformationManager
 import jp.faketuna.minecraft2fa.waterfall.manager.PluginInstanceManager
 import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.chat.TextComponent
+import net.md_5.bungee.api.connection.Server
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class AuthSuccessEventListener: Listener {
 
     @EventHandler
-    fun onAuthSuccess(event: AuthSuccessEvent){
+    fun onAuthSuccess(event: AuthSuccessEvent) {
         // TODO PluginMessaging 処理
         AuthInformationManager().addAuthorizedUser(event.getUUID())
-        ProxyServer.getInstance().scheduler.schedule(PluginInstanceManager().getPlugin(), Runnable {
-            AuthInformationManager().addAuthorizeExpiredUser(event.getUUID())
-        }, PluginInstanceManager().getConfigManager(PluginInstanceManager().getPlugin()).getSessionExpireTime(), TimeUnit.SECONDS)
+        val player = ProxyServer.getInstance().getPlayer(event.getUUID())
+        if (player != null){
+            println("player has in")
+            player.sendMessage(TextComponent("§aYour now verified and can execute moderation commands!"))
+            sendAuthData(player.server, player.uniqueId)
+        }
+        ProxyServer.getInstance().scheduler.schedule(
+            PluginInstanceManager().getPlugin(),
+            Runnable {
+                AuthInformationManager().addAuthorizeExpiredUser(event.getUUID())
+            },
+            PluginInstanceManager().getConfigManager(PluginInstanceManager().getPlugin()).getSessionExpireTime(),
+            TimeUnit.SECONDS
+        )
+    }
+
+    private fun sendAuthData(server: Server, uuid: UUID){
+        val networkPlayers = ProxyServer.getInstance().players
+        if (networkPlayers.isNullOrEmpty()) return
+
+        val outStream = ByteStreams.newDataOutput()
+        outStream.writeUTF("authInformationShare")
+        outStream.writeUTF(uuid.toString())
+        outStream.writeBoolean(AuthInformationManager().isUserAuthorized(uuid))
+
+        server.sendData("mc2fa:authentication", outStream.toByteArray())
     }
 }
